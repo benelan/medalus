4a2832b1087b3911e3ae76cab4671af4f3401f2a
benelan <benelan@yahoo.com>
2020-01-10 20:59:27 -0800

    map component

--- client/src/App.js
+++ client/src/App.js
@@ -1,43 +1,18 @@
 import React from 'react';
-import { loadCss } from 'esri-loader'
-import { loadMap } from './esriMap/'
 import NavBar from './components/NavBar';
 import UserInputForm from './components/UserInputForm';
 import About from './components/About';
+import EsriMap from './components/EsriMap';
 
-class App extends React.Component {
-  componentWillMount() {
-    loadCss();
-    loadMap();
-  }
-  render() {
-    const mD = {
-      width: "100%",
-      height: "600px"
-    };
-
+const App = () => {
     return (
       <React.Fragment>
         <NavBar />
-        <div id="viewDiv" style={mD}>
-          <button id="feature-count" class="esri-widget">
-            Number of Features
-          </button>
-          <span id="select-county-title">Select a county:</span>
-          <br />
-          <select id="county" class="esri-select">
-            <option value="San Bernardino">San Bernardino</option>
-            <option value="Los Angeles">Los Angeles</option>
-            <option value="San Francisco">San Francisco</option>
-            <option value="Santa Clara">Santa Clara</option>
-            <option value="San Diego">San Diego</option>
-          </select>
-        </div>
+        <EsriMap />
         <UserInputForm />
         <About />
       </React.Fragment>
     );
   }
-}
 
 export default App;
