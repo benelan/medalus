8af149a6676e8998dd28241c983389ef1e5242f8
benelan <benelan@yahoo.com>
2020-01-12 19:24:34 -0800

    about columns

--- client/src/components/About.js
+++ client/src/components/About.js
@@ -10,67 +10,109 @@ class About extends Component {
         return (
             <Row style={homeStyle} id="about">
                 <Col md={12}>
-                    <Row style={homeStyle}>
-
-                        <Col md={5}>
-                            <h4 className="text-center">Medalus</h4>
-                            <h5 className="text-center"><i>Mediterranean Desertification and Land Use</i></h5>
-                            <p>Desertification indicatiors can demonstrate that desertification has already reached to an irreversible point. However, the indicators also demonstrate a potential risk
-                                of desertification in certain areas where there is still time to reverse the trend. The Medalus method can help guide organization to focus on the regions 
+                    <h4 className="text-center">Medalus</h4>
+                    <h5 className="text-center"><i>Mediterranean Desertification and Land Use</i></h5>
+                    <p>Desertification indicatiors can demonstrate that desertification has already reached to an irreversible point. However, the indicators also demonstrate a potential risk
+                        of desertification in certain areas where there is still time to reverse the trend. The Medalus method can help guide organization to focus on the regions
                             </p>
 
-                            <p>We searched for a method or model in the scientific community for a way to measure the desertification process, and that is when we found a few scholarly journals referencing
-                                the Medalus method. The journals used the method to measure desertification in Morocco, Serbia, Mongolia, and Algeria. We decided to use this method and the journals to measure
-                                desertification in the United States, in particular, California.
+                    <p>We searched for a method or model in the scientific community for a way to measure the desertification process, and that is when we found a few scholarly journals referencing
+                        the Medalus method. The journals used the method to measure desertification in Morocco, Serbia, Mongolia, and Algeria. We decided to use this method and the journals to measure
+                        desertification in the United States, in particular, California.
                             </p>
-
-                            <h4 className="text-center">SQI</h4>
-                            <h5 className="text-center"><i>SQI = (S x Dp x B x T x Dr)<sup>1/5</sup></i></h5>
-                            <img src={process.env.PUBLIC_URL + './SQI.PNG'} alt="SQI"></img>
-
-                            <h4 className="text-center">VQI</h4>
-                            <h5 className="text-center"><i>VQI = (Ep x Dr x Pc)<sup>1/3</sup></i></h5>
-                            <img src={process.env.PUBLIC_URL + './VQI.PNG'} alt="VQI"></img>
-
-                            <h4 className="text-center">CQI</h4>
-                            <h5 className="text-center"><i>CQI = (R x AI)<sup>1/2</sup></i></h5>
-                            <img src={process.env.PUBLIC_URL + './CQI.PNG'} alt="CQI"></img>
-
-                            <h4 className="text-center">MQI</h4>
-                            <h5 className="text-center"><i>MQI = (Human pressure x Grazing pressure)<sup>1/2</sup></i></h5>
-                            <img src={process.env.PUBLIC_URL + './MQI.PNG'} alt="MQI"></img>
-
-                            <h4 className="text-center">ESAI</h4>
-                            <h5 className="text-center"><i>ESAI = (SQI x CQI x VQI x MQI)<sup>1/4</sup></i></h5>
-                            <img src={process.env.PUBLIC_URL + './Desertificate2018.PNG'} alt="Desertificate2018"></img>
-                            
-                            
-                            <h4 className="text-center" style={{paddingTop: '20px'}}>Setting up the Data</h4>
-                            <p>Instructions to set up the data</p>
-                            <p>Commodo viverra maecenas accumsan lacus vel facilisis volutpat. Id cursus metus aliquam eleifend mi in nulla posuere sollicitudin. Venenatis lectus magna fringilla urna porttitor rhoncus dolor purus non. Lorem ipsum dolor sit amet consectetur adipiscing. Aliquet sagittis id consectetur purus ut. Quam vulputate dignissim suspendisse in est ante. Elementum nisi quis eleifend quam. Tempor id eu nisl nunc. Integer enim neque volutpat ac tincidunt vitae. Arcu felis bibendum ut tristique et. Dictum sit amet justo donec enim diam vulputate.</p>
-
-                            <p>Nunc vel risus commodo viverra. Vitae sapien pellentesque habitant morbi tristique. Diam maecenas ultricies mi eget mauris pharetra et ultrices. Id diam vel quam elementum. Sed faucibus turpis in eu mi bibendum neque egestas. Turpis in eu mi bibendum. Amet venenatis urna cursus eget nunc scelerisque viverra. Tortor vitae purus faucibus ornare. Suscipit adipiscing bibendum est ultricies integer quis. Morbi tristique senectus et netus et. Aliquet porttitor lacus luctus accumsan tortor posuere ac ut consequat. Posuere sollicitudin aliquam ultrices sagittis orci a.</p>
-
-                            <p>Faucibus vitae aliquet nec ullamcorper sit. Diam ut venenatis tellus in metus vulputate eu scelerisque felis. Nibh venenatis cras sed felis. Sit amet dictum sit amet justo. Aliquet eget sit amet tellus. Faucibus turpis in eu mi bibendum neque egestas congue. Faucibus nisl tincidunt eget nullam non. Nisi est sit amet facilisis magna etiam. Quisque non tellus orci ac auctor augue mauris augue neque. Lorem ipsum dolor sit amet consectetur. Vitae nunc sed velit dignissim. Facilisis sed odio morbi quis. Elementum sagittis vitae et leo duis ut diam quam. Eu tincidunt tortor aliquam nulla facilisi cras.</p>
-                        </Col>
-
-                        <Col md={{ size: 5, offset: 2 }}>
-                            <h4 className="text-center">Desertification</h4>
-
-                            <p>Talk about what Desertification is and how we calculated it</p>
-
-                            <p>Nunc vel risus commodo viverra. Vitae sapien pellentesque habitant morbi tristique. Diam maecenas ultricies mi eget mauris pharetra et ultrices. Id diam vel quam elementum. Sed faucibus turpis in eu mi bibendum neque egestas. Turpis in eu mi bibendum. Amet venenatis urna cursus eget nunc scelerisque viverra. Tortor vitae purus faucibus ornare. Suscipit adipiscing bibendum est ultricies integer quis. Morbi tristique senectus et netus et. Aliquet porttitor lacus luctus accumsan tortor posuere ac ut consequat. Posuere sollicitudin aliquam ultrices sagittis orci a.</p>
-
-                            <p>Faucibus vitae aliquet nec ullamcorper sit. Diam ut venenatis tellus in metus vulputate eu scelerisque felis. Nibh venenatis cras sed felis. Sit amet dictum sit amet justo. Aliquet eget sit amet tellus. Faucibus turpis in eu mi bibendum neque egestas congue. Faucibus nisl tincidunt eget nullam non. Nisi est sit amet facilisis magna etiam. Quisque non tellus orci ac auctor augue mauris augue neque. Lorem ipsum dolor sit amet consectetur. Vitae nunc sed velit dignissim. Facilisis sed odio morbi quis. Elementum sagittis vitae et leo duis ut diam quam. Eu tincidunt tortor aliquam nulla facilisi cras.</p>
-
-                            
-                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Sed velit dignissim sodales ut eu sem integer vitae. Cursus mattis molestie a iaculis at erat pellentesque adipiscing. Eu augue ut lectus arcu. A erat nam at lectus urna duis convallis convallis. Fringilla phasellus faucibus scelerisque eleifend donec pretium. Id aliquet risus feugiat in ante metus dictum. Pellentesque sit amet porttitor eget dolor morbi non. At lectus urna duis convallis convallis tellus. Felis eget velit aliquet sagittis. Tempus imperdiet nulla malesuada pellentesque elit eget gravida cum. In nulla posuere sollicitudin aliquam ultrices sagittis orci a. Platea dictumst quisque sagittis purus. Neque gravida in fermentum et. Non blandit massa enim nec dui nunc mattis. Viverra aliquet eget sit amet.</p>
-
-                            <p>Nisl nisi scelerisque eu ultrices vitae auctor eu augue. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Ut diam quam nulla porttitor. Justo eget magna fermentum iaculis eu non diam. Eget velit aliquet sagittis id consectetur. Sed risus pretium quam vulputate dignissim suspendisse. Volutpat ac tincidunt vitae semper quis lectus nulla at. Urna porttitor rhoncus dolor purus non. Cursus euismod quis viverra nibh. Cursus in hac habitasse platea dictumst quisque sagittis purus. Diam donec adipiscing tristique risus. Lorem ipsum dolor sit amet consectetur adipiscing. Habitasse platea dictumst vestibulum rhoncus est pellentesque. Eu consequat ac felis donec et. In hendrerit gravida rutrum quisque non tellus orci ac auctor.</p>
-                        </Col>
-                    </Row>
+                    <br></br>
                 </Col>
-            </Row>
+                <Row>
+                    <Col md={6}>
+                        <h4 className="text-center">SQI</h4>
+                        <h5 className="text-center"><i>SQI = (S x Dp x B x T x Dr)<sup>1/5</sup></i></h5>
+                        <img src={process.env.PUBLIC_URL + './SQI.PNG'} alt="SQI"></img>
+
+                    </Col>
+                    <Col md={6}>
+                        <p>SQI</p>
+                        <p>Commodo viverra maecenas accumsan lacus vel facilisis volutpat. Id cursus metus aliquam eleifend mi in nulla posuere sollicitudin. Venenatis lectus magna fringilla urna porttitor rhoncus dolor purus non. Lorem ipsum dolor sit amet consectetur adipiscing. Aliquet sagittis id consectetur purus ut. Quam vulputate dignissim suspendisse in est ante. Elementum nisi quis eleifend quam. Tempor id eu nisl nunc. Integer enim neque volutpat ac tincidunt vitae. Arcu felis bibendum ut tristique et. Dictum sit amet justo donec enim diam vulputate.</p>
+                    </Col>
+                </Row>
+
+                <Row>
+                    <Col md={6}>
+                        <h4 className="text-center">VQI</h4>
+                        <h5 className="text-center"><i>VQI = (Ep x Dr x Pc)<sup>1/3</sup></i></h5>
+                        <img src={process.env.PUBLIC_URL + './VQI.PNG'} alt="VQI"></img>
+                    </Col>
+                    <Col md={6}>
+                        <p>VQI</p>
+                        <p>Commodo viverra maecenas accumsan lacus vel facilisis volutpat. Id cursus metus aliquam eleifend mi in nulla posuere sollicitudin. Venenatis lectus magna fringilla urna porttitor rhoncus dolor purus non. Lorem ipsum dolor sit amet consectetur adipiscing. Aliquet sagittis id consectetur purus ut. Quam vulputate dignissim suspendisse in est ante. Elementum nisi quis eleifend quam. Tempor id eu nisl nunc. Integer enim neque volutpat ac tincidunt vitae. Arcu felis bibendum ut tristique et. Dictum sit amet justo donec enim diam vulputate.</p>
+                    </Col>
+                </Row>
+
+                <Row>
+                    <Col md={6}>
+                        <h4 className="text-center">CQI</h4>
+                        <h5 className="text-center"><i>CQI = (R x AI)<sup>1/2</sup></i></h5>
+                        <img src={process.env.PUBLIC_URL + './CQI.PNG'} alt="CQI"></img>
+
+                    </Col>
+                    <Col md={6}>
+                        <p>CQI</p>
+                        <p>Commodo viverra maecenas accumsan lacus vel facilisis volutpat. Id cursus metus aliquam eleifend mi in nulla posuere sollicitudin. Venenatis lectus magna fringilla urna porttitor rhoncus dolor purus non. Lorem ipsum dolor sit amet consectetur adipiscing. Aliquet sagittis id consectetur purus ut. Quam vulputate dignissim suspendisse in est ante. Elementum nisi quis eleifend quam. Tempor id eu nisl nunc. Integer enim neque volutpat ac tincidunt vitae. Arcu felis bibendum ut tristique et. Dictum sit amet justo donec enim diam vulputate.</p>
+                    </Col>
+                </Row>
+
+
+                <Row>
+                    <Col md={6}>
+                        <h4 className="text-center">MQI</h4>
+                        <h5 className="text-center"><i>MQI = (Human pressure x Grazing pressure)<sup>1/2</sup></i></h5>
+                        <img src={process.env.PUBLIC_URL + './MQI.PNG'} alt="MQI"></img>
+                    </Col>
+                    <Col md={6}>
+                        <p>MQI</p>
+                        <p>Commodo viverra maecenas accumsan lacus vel facilisis volutpat. Id cursus metus aliquam eleifend mi in nulla posuere sollicitudin. Venenatis lectus magna fringilla urna porttitor rhoncus dolor purus non. Lorem ipsum dolor sit amet consectetur adipiscing. Aliquet sagittis id consectetur purus ut. Quam vulputate dignissim suspendisse in est ante. Elementum nisi quis eleifend quam. Tempor id eu nisl nunc. Integer enim neque volutpat ac tincidunt vitae. Arcu felis bibendum ut tristique et. Dictum sit amet justo donec enim diam vulputate.</p>
+                    </Col>
+                </Row>
+
+                <Row>
+                    <Col md={6}>
+
+                        <h4 className="text-center">ESAI</h4>
+                        <h5 className="text-center"><i>ESAI = (SQI x CQI x VQI x MQI)<sup>1/4</sup></i></h5>
+                        <img src={process.env.PUBLIC_URL + './Desertificate2018.PNG'} alt="Desertificate2018"></img>
+
+                    </Col>
+                    <Col md={6}>
+                        <p>ESAI</p>
+                        <p>Commodo viverra maecenas accumsan lacus vel facilisis volutpat. Id cursus metus aliquam eleifend mi in nulla posuere sollicitudin. Venenatis lectus magna fringilla urna porttitor rhoncus dolor purus non. Lorem ipsum dolor sit amet consectetur adipiscing. Aliquet sagittis id consectetur purus ut. Quam vulputate dignissim suspendisse in est ante. Elementum nisi quis eleifend quam. Tempor id eu nisl nunc. Integer enim neque volutpat ac tincidunt vitae. Arcu felis bibendum ut tristique et. Dictum sit amet justo donec enim diam vulputate.</p>
+                    </Col>
+                </Row>
+                <Row style={homeStyle}>
+                    <Col md={5}>
+                        <h4 className="text-center" style={{ paddingTop: '20px' }}>Setting up the Data</h4>
+                        <p>Instructions to set up the data</p>
+                        <p>Commodo viverra maecenas accumsan lacus vel facilisis volutpat. Id cursus metus aliquam eleifend mi in nulla posuere sollicitudin. Venenatis lectus magna fringilla urna porttitor rhoncus dolor purus non. Lorem ipsum dolor sit amet consectetur adipiscing. Aliquet sagittis id consectetur purus ut. Quam vulputate dignissim suspendisse in est ante. Elementum nisi quis eleifend quam. Tempor id eu nisl nunc. Integer enim neque volutpat ac tincidunt vitae. Arcu felis bibendum ut tristique et. Dictum sit amet justo donec enim diam vulputate.</p>
+
+                        <p>Nunc vel risus commodo viverra. Vitae sapien pellentesque habitant morbi tristique. Diam maecenas ultricies mi eget mauris pharetra et ultrices. Id diam vel quam elementum. Sed faucibus turpis in eu mi bibendum neque egestas. Turpis in eu mi bibendum. Amet venenatis urna cursus eget nunc scelerisque viverra. Tortor vitae purus faucibus ornare. Suscipit adipiscing bibendum est ultricies integer quis. Morbi tristique senectus et netus et. Aliquet porttitor lacus luctus accumsan tortor posuere ac ut consequat. Posuere sollicitudin aliquam ultrices sagittis orci a.</p>
+
+                        <p>Faucibus vitae aliquet nec ullamcorper sit. Diam ut venenatis tellus in metus vulputate eu scelerisque felis. Nibh venenatis cras sed felis. Sit amet dictum sit amet justo. Aliquet eget sit amet tellus. Faucibus turpis in eu mi bibendum neque egestas congue. Faucibus nisl tincidunt eget nullam non. Nisi est sit amet facilisis magna etiam. Quisque non tellus orci ac auctor augue mauris augue neque. Lorem ipsum dolor sit amet consectetur. Vitae nunc sed velit dignissim. Facilisis sed odio morbi quis. Elementum sagittis vitae et leo duis ut diam quam. Eu tincidunt tortor aliquam nulla facilisi cras.</p>
+                    </Col>
+
+                    <Col md={{ size: 5, offset: 2 }}>
+                        <h4 className="text-center">Desertification</h4>
+
+                        <p>Talk about what Desertification is and how we calculated it</p>
+
+                        <p>Nunc vel risus commodo viverra. Vitae sapien pellentesque habitant morbi tristique. Diam maecenas ultricies mi eget mauris pharetra et ultrices. Id diam vel quam elementum. Sed faucibus turpis in eu mi bibendum neque egestas. Turpis in eu mi bibendum. Amet venenatis urna cursus eget nunc scelerisque viverra. Tortor vitae purus faucibus ornare. Suscipit adipiscing bibendum est ultricies integer quis. Morbi tristique senectus et netus et. Aliquet porttitor lacus luctus accumsan tortor posuere ac ut consequat. Posuere sollicitudin aliquam ultrices sagittis orci a.</p>
+
+                        <p>Faucibus vitae aliquet nec ullamcorper sit. Diam ut venenatis tellus in metus vulputate eu scelerisque felis. Nibh venenatis cras sed felis. Sit amet dictum sit amet justo. Aliquet eget sit amet tellus. Faucibus turpis in eu mi bibendum neque egestas congue. Faucibus nisl tincidunt eget nullam non. Nisi est sit amet facilisis magna etiam. Quisque non tellus orci ac auctor augue mauris augue neque. Lorem ipsum dolor sit amet consectetur. Vitae nunc sed velit dignissim. Facilisis sed odio morbi quis. Elementum sagittis vitae et leo duis ut diam quam. Eu tincidunt tortor aliquam nulla facilisi cras.</p>
+
+
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Sed velit dignissim sodales ut eu sem integer vitae. Cursus mattis molestie a iaculis at erat pellentesque adipiscing. Eu augue ut lectus arcu. A erat nam at lectus urna duis convallis convallis. Fringilla phasellus faucibus scelerisque eleifend donec pretium. Id aliquet risus feugiat in ante metus dictum. Pellentesque sit amet porttitor eget dolor morbi non. At lectus urna duis convallis convallis tellus. Felis eget velit aliquet sagittis. Tempus imperdiet nulla malesuada pellentesque elit eget gravida cum. In nulla posuere sollicitudin aliquam ultrices sagittis orci a. Platea dictumst quisque sagittis purus. Neque gravida in fermentum et. Non blandit massa enim nec dui nunc mattis. Viverra aliquet eget sit amet.</p>
+
+                        <p>Nisl nisi scelerisque eu ultrices vitae auctor eu augue. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Ut diam quam nulla porttitor. Justo eget magna fermentum iaculis eu non diam. Eget velit aliquet sagittis id consectetur. Sed risus pretium quam vulputate dignissim suspendisse. Volutpat ac tincidunt vitae semper quis lectus nulla at. Urna porttitor rhoncus dolor purus non. Cursus euismod quis viverra nibh. Cursus in hac habitasse platea dictumst quisque sagittis purus. Diam donec adipiscing tristique risus. Lorem ipsum dolor sit amet consectetur adipiscing. Habitasse platea dictumst vestibulum rhoncus est pellentesque. Eu consequat ac felis donec et. In hendrerit gravida rutrum quisque non tellus orci ac auctor.</p>
+                    </Col>
+                </Row>
+            </Row >
         );
     }
 }
